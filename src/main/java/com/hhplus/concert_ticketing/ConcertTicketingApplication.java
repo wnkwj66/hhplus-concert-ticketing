785020cd6031eb9e3352b08313fd436f1978585e
wjkim <wnkwj66@gmail.com>
2024-10-15 15:14:37 +0900

    Initial commit

---
+++ src/main/java/com/hhplus/concert_ticketing/ConcertTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.concert_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhplus/concert_ticketing/ConcertTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.concert_ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
