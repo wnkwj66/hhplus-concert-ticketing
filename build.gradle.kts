3e5635be855254c3e7763bde5ae770084cb27136
wjkim <wnkwj66@gmail.com>
2024-10-24 17:15:09 +0900

    feat: JWT 토큰 생성을 위한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,15 +24,25 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	// swagger
 	implementation("io.swagger.core.v3:swagger-annotations:2.1.6")
-	runtimeOnly("com.h2database:h2")
+	// lombok
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+	// JWT
+	implementation ("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5") // 필요한 경우 JSON 처리용
+	// DB
+	runtimeOnly("com.h2database:h2")
+	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+
 }
 
 tasks.withType<Test> {
