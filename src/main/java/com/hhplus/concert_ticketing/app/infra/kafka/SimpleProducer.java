226b85c8adeff7d26cf04d6b480bcf2ef9f42a32
wjkim <wnkwj66@gmail.com>
2024-11-21 17:19:39 +0900

    feat: 카프카 스프링 연동

---
+++ src/main/java/com/hhplus/concert_ticketing/app/infra/kafka/SimpleProducer.java
@@ -0,0 +1,30 @@
+package com.hhplus.concert_ticketing.app.infra.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class SimpleProducer {
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        ProducerRecord record = new ProducerRecord(topic, message);
+        kafkaTemplate.send(record);
+    }
+
+    public void sendMessage(String topic, String key, String message) {
+        ProducerRecord record = new ProducerRecord(topic, key, message);
+        kafkaTemplate.send(record);
+    }
+
+    public void sendMessage(String topic, int partition, String key, String message) {
+        ProducerRecord record = new ProducerRecord(topic, partition, key, message);
+        kafkaTemplate.send(record);
+    }
+}
