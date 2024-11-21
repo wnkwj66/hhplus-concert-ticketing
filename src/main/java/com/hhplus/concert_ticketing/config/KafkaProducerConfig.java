0459c327a3d7d61dfbc5738ec52eba0ebaad5a1a
wjkim <wnkwj66@gmail.com>
2024-11-22 08:18:44 +0900

    chore: 카프카 설정파일 수정

--- src/main/java/com/hhplus/concert_ticketing/config/KafkaProducerConfig.java
+++ src/main/java/com/hhplus/concert_ticketing/config/KafkaProducerConfig.java
@@ -1,10 +1,13 @@
 package com.hhplus.concert_ticketing.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -15,6 +18,18 @@ import java.util.Map;
 @Slf4j
 @Configuration
 public class KafkaProducerConfig {
+   @Value("${spring.kafka.topic.payment")
+    private String paymentTopic;
+
+    // Payment Topic 생성
+    @Bean
+    public NewTopic paymentTopic() {
+        return TopicBuilder.name(paymentTopic)
+                .partitions(1) // 파티션 개수
+                .replicas(1)   // 복제본 개수
+                .build();
+    }
+
     @Bean
     public ProducerFactory<String, String> kafkaProducerFactory() {
         Map<String, Object> props = new HashMap<>();
